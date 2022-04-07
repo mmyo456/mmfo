c2015d7c012dbe139c6334725db21dc02068d46f
Mingming8886 <86056701+Mingming8886@users.noreply.github.com>
2022-04-07 22:07:41 +0800

    Upload by PicGo

---
+++ script1.03.js
@@ -0,0 +1,27 @@
+// Declare the Elements 
+const dispNum = document.querySelector(".display .num");
+const dispErr = document.querySelector(".container .stg");
+
+
+window.onload = function () {
+
+    function showNum () {
+        const randomNum = Math.floor(Math.random() * 1000);
+        const randomStr = randomNum.toString()
+
+        dispNum.textContent = randomStr
+    }
+
+    var interval =  setInterval( showNum , 500);
+
+    
+    
+    setTimeout(()=> {
+        clearInterval(interval);
+
+        dispNum.textContent = "警告";
+        dispErr.style.display = "block";
+        dispErr.textContent = "普通网站访客，请联系网站管理员"
+        dispmmy.textContent = "普通网rr站访客，请联系网站管理员"
+    }, 4000);
+}
